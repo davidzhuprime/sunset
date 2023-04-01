b9afa89d7bdca1caf3b5c7f36fa11b2c38a89363
davidzhuprime <davidzhuprime@yahoo.com>
2023-04-01 23:40:33 +0800

    Add package-lock.json

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+
+// Define a route that plays music
+app.get('/play', (req, res) => {
+  res.send(`
+    <audio controls>
+      <source src="/kuckuck.mp3" type="audio/mpeg">
+      Your browser does not support the audio element.
+    </audio>
+  `);
+});
 // Start the server
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
